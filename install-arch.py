17042d3a27327f52e97ad8afbaf454904171e19f
Leonardo Banderali <leonardo2718@protonmail.com>
2017-07-07 18:23:51 -0400

    Add custom Arch installation script

---
+++ install-arch.py
@@ -0,0 +1,5 @@
+#! /usr/bin/python
+
+base = ["base", "base-devel", "python", "vim", "git", "reflector", "openssh", "zsh", "ttf-inconsolata", "vim-colorsamplerpack"]
+devel= ["clang", "qt5", "qtcreator"]
+guiapps = ["termite"]
